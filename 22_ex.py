ddeaa1c977aba9025bdf3297cc2808dd666cb877
EgeDeniz <163152609+denizege@users.noreply.github.com>
2024-06-25 22:07:57 +0300

    Create 22_ex.py

---
+++ 22_ex.py
@@ -0,0 +1,7 @@
+# 1 Получаем гласные
+
+def get_vowels(String):
+    return [each for each in String if each in "aeiou"]
+get_vowels("animal") # [a, i, a]
+get_vowels("sky") # []
+get_vowels("football") # [o, o, a]
